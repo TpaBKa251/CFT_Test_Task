1d93f6c2a96d73a314bf0a0e71e47bce6f0dd143
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-08-21 21:41:37 +0700

    Попытки исправить отображение в консоли

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.fusesource.jansi.AnsiConsole;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Вся программа поделена на классы:
  * <p>
@@ -24,6 +27,9 @@ import org.fusesource.jansi.AnsiConsole;
  */
 public class Main {
     public static void main(String[] args) {
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+        System.setErr(new PrintStream(System.err, true, StandardCharsets.UTF_8));
+
         // Инициализация Jansi
         AnsiConsole.systemInstall();
 
